f9e0a42ad8772da7f0fdb1297645138873f4762f
floring <floring@users.noreply.github.com>
2015-08-25 18:36:17 +0300

    Implement methods checks the input string is opening or closing bracket.

--- src/com/arles/smartcalculator/StringUtil.java
+++ src/com/arles/smartcalculator/StringUtil.java
@@ -15,5 +15,23 @@ public class StringUtil {
 	public static String trimWhitespaces(String string) {
 		return string.replaceAll(SPACE_CHAR, "");
 	}
+	
+	public static boolean isNumber(String inputString) {
+		for(int i = 0; i < inputString.length(); ++i) { 
+			if(!Character.isDigit(inputString.charAt(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static boolean isOpenRoundBracket(String inputString) {
+		return inputString.equals("(");
+	}
+	
+	public static boolean isCloseRoundBracket(String inputString) {
+		return inputString.equals(")");
+		
+	}
 
 }
